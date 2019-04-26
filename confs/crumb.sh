47a643021be3f37734bcdd342b771873dd28d3e3
Marcos Tolosa <marcostolosa@protonmail.com>
2019-04-26 20:41:15 -0300

    Update crumb.sh

--- confs/crumb.sh
+++ confs/crumb.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# Eg. "http://jenkins.local:8080/"
+# Eg. "http://jenkins.local:8080"
 URL="$1"
 # Eg. "PARAM"
-PARAM="$2"
+PARAM="$5"
 # Eg. "production-job"
-JOBS=$3
-# Eg. "?MYSQL_HOST=51.13.458.1&DB_NAME=db_production&S3_BUCKET=s3_production"
-PARAMS="$4"
+JOBS=$4
+# Eg. "MYSQL_HOST=51.13.458.1&DB_NAME=db_production&S3_BUCKET=s3_production"
+PARAMS="$6"
 
-USER=$5
-PASS=$6
+USER=$2
+PASS=$3
 
 if [ "$1" == ""] && [ "$2" == ""]; then
-  echo "Usage: $0 <JENKINS_URL> [PARAM] [QUERY_PARAMETERS]"
-  echo "Eg. # $0 'https://jenkins.server.com/' '?MYSQL_HOST=db_host.com&DB_NAME=db_production'"
+  echo "Usage: $0 <JENKINS_URL> <USER> <PASS> <JOB_NAME> [PARAM] [QUERY_PARAMETERS] "
+  echo "Eg. # $0 'https://jenkins.server.com' 'MYSQL_HOST=db_host.com&DB_NAME=db_production'"
   exit 0
 else
   # Get the Crumb to Pass as Header
-  crumb=$(curl -u "user:password" -s 'http://$URL:8080/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
+  crumb=$(curl -u "$USER:$PASS" -s '$URL/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
 
   if [ "$PARAM" == "PARAM" ] && [ "$3" != "" ]; then
     # Trigger a Parameter Job
-    curl -u "$USER:$PASS" -H "$crumb" -X POST  http://$URL/job/$JOBS/buildWithParameters$PARAMS
+    curl -u "$USER:$PASS" -H "$crumb" -X POST  "$URL/job/$JOBS/buildWithParameters?$PARAMS"
   else
     # Trigger Common Jobs
-    curl -u "$USER:$PASS" -H "$crumb" -X POST http://$URL/job/$JOBS/build?delay=0sec
+    curl -u "$USER:$PASS" -H "$crumb" -X POST "$URL/job/$JOBS/build?delay=0sec"
   fi
 fi
